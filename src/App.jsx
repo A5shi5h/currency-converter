6ca3bc7f7551012d5a21a15649cf0f3f85143356
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2024-04-28 13:50:14 +0530

    changes in buttons

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,27 @@ function App() {
   }
 
   const convert = () => {
-    setConvertedAmount(amount * currencyInfo[to])
+
+    if(amount > 0){
+      setConvertedAmount(amount * currencyInfo[to])   
+    }else{
+      alert("please enter amount grater than 0")
+    }
+    
   }
   
   return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuYtTFuGUbyGHVLqXgQqvDwRtNnJgZdIMNfHoX_qxzww&s')`,
+            backgroundImage: `url('https://www.desktopbackground.org/download/o/2010/12/30/134377_desktop-themes-black-currency-money-wallpapers-1792862_2560x1440_h.jpg')`,
             backgroundRepeat: `no-repeat`,
             backgroundSize : `cover`,
             backgroundPosition:`center`
         }}
     >
         <div className="w-full">
-            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <div className="w-full max-w-sm mx-auto border-2 border-gray-60 rounded-lg p-5 backdrop-blur-0 bg-white/25">
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
@@ -44,7 +50,7 @@ function App() {
                        
                     }}
                 >
-                    <div className="w-full mb-1">
+                    <div className="w-full mb-1 border-4 border-blue-500 rounded-xl">
                         <InputBox
                             label="From"
                             amount={amount}
@@ -57,13 +63,13 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 hover:bg-blue-500 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-red-600 hover:bg-blue-500 text-white px-2 py-0.5"
                             onClick={swap}  
                         >
                             swap
                         </button>
                     </div>
-                    <div className="w-full mt-1 mb-4">
+                    <div className="w-full mt-1 mb-4 border-4 border-blue-500 rounded-xl">
                         <InputBox
                             label="To"
                             amount={convertedAmount}
@@ -73,7 +79,8 @@ function App() {
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-500 hover:text-black">
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 
+                    border-2 border-white rounded-lg hover:bg-red-500 hover:text-black">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
